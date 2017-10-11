78add4c9ab24f9226e82523cc6d8fb998a306e35
surgeharb <me@sergeharb.com>
2017-10-11 22:32:23 +0300

    modify configuration pins

--- config/configuration.ts
+++ config/configuration.ts
@@ -1,17 +1,17 @@
 export let RGBLed = {
   pins: {
-    "red": 11,
-    "green": 10,
-    "blue": 9
+    'red': 11,
+    'green': 10,
+    'blue': 9
   }
 }
 
 export let DiscoLeds = {
   pins: {
-    "red": 5,
-    "blue": 2,
-    "green": 3,
-    "white": 8,
-    "yellow": 6
+    'red': 5,
+    'blue': 2,
+    'green': 3,
+    'white': 8,
+    'yellow': 6
   }
 }
